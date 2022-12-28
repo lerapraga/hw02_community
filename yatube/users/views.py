e8ea930bbc4417d63207dadb00e26f1106bc18b5
Lera Praga <abastrenie@mail.ru>
2022-12-28 21:09:08 +0500

    'авторизация и логаут настроены'

---
+++ yatube/users/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm  # из какого класса взять форму
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig'  # Добавленное приложение
+    'users.apps.UsersConfig',
+    'posts.apps.PostsConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -90,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -105,3 +107,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+# LOGOUT_REDIRECT_URL = 'posts:index'
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
